cc9d6c88ae1909a8e1110850f760ab59b24ef0cb
Bill Maxwell <cloudnautique@users.noreply.github.com>
2023-12-08 17:18:04 -0700

    Updates
    Use new internal credential helper.
    Support admin user to match container service interface.
    support idempotent running of the create.sh script
    
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,15 +4,14 @@
 echo "-> [create.sh]"
 
 # Make sure this script only repply on an Acorn creation event
-if [ "${ACORN_EVENT}" != "create" ]; then
-   echo "ACORN_EVENT must be [create], currently is [${ACORN_EVENT}]"
+if [ "${ACORN_EVENT}" == "delete" ]; then
+   echo "ACORN_EVENT must be [create,update], currently is [${ACORN_EVENT}]"
    exit 0
 fi
 
-# Check if cluster with that name already exit
-atlas cluster get ${CLUSTER_NAME} 2>/dev/null
-if [ $? -eq 0 ]; then
-  echo "-> cluster ${CLUSTER_NAME} already exists"
+render_service() {
+  local CLUSTER_NAME=$1
+  local db_name=${DB_NAME}
 
   DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
   DB_PROTO=$(echo $DB_ADDRESS | cut -d':' -f1)
@@ -22,40 +21,45 @@ if [ $? -eq 0 ]; then
   cat > /run/secrets/output<<EOF
   services: atlas: {
     address: "${DB_HOST}"
-    secrets: ["user"]
+    default: true
+    secrets: ["admin", "user"]
     ports: "27017"
     data: {
       proto: "${DB_PROTO}"
-      dbName: "${DB_NAME}"
+      dbName: "${db_name}"
     }
   }
 EOF
-  exit 0
-else
-  echo "-> cluster ${CLUSTER_NAME} does not exist"
-fi
+}
 
-# Create a cluster in the current project
-echo "-> about to create cluster ${CLUSTER_NAME} of type ${TIER} in ${PROVIDER} / ${REGION}"
-result=$(atlas cluster create ${CLUSTER_NAME} --region $REGION --provider $PROVIDER --tier $TIER --tag creator=acorn_service --mdbVersion $DB_VERSION)
-
-# Make sure the cluster was created correctly
+# Check if cluster with that name already exit
+atlas cluster get ${CLUSTER_NAME} 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo $result
-  exit 1
-fi
+  echo "-> cluster ${CLUSTER_NAME} does not exist"
 
-# Wait for Atlas to provide cluster's connection string
-echo "-> waiting for database address"
-while true; do
-  DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
-  if [ "${DB_ADDRESS}" = "null" ]; then
-      sleep 2
-      echo "... retrying"
-  else
-    break
+  # Create a cluster in the current project
+  echo "-> about to create cluster ${CLUSTER_NAME} of type ${TIER} in ${PROVIDER} / ${REGION}"
+  result=$(atlas cluster create ${CLUSTER_NAME} --region $REGION --provider $PROVIDER --tier $TIER --tag creator=acorn_service --mdbVersion $DB_VERSION)
+  
+  # Make sure the cluster was created correctly
+  if [ $? -ne 0 ]; then
+    echo $result
+    exit 1
   fi
-done
+  
+  # Wait for Atlas to provide cluster's connection string
+  echo "-> waiting for database address"
+  while true; do
+    DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
+    if [ "${DB_ADDRESS}" = "null" ]; then
+        sleep 2
+        echo "... retrying"
+    else
+      break
+    fi
+  done
+
+fi
 
 # Allow database network access from current IP
 echo "-> allowing connection from current IP address"
@@ -64,26 +68,44 @@ if [ $? -ne 0 ]; then
   echo $res
 fi
 
-# Create db user
-echo "-> creating a database user"
-res=$(atlas dbusers create --username ${DB_USER} --password ${DB_PASS} --role readWrite@${DB_NAME})
-if [ $? -ne 0 ]; then
-  echo $res
+db_user_update() {
+  res=$(atlas dbusers update $@)
+  if [ $? -ne 0 ]; then
+    echo $res
+  fi
+}
+
+db_user_create() {
+  res=$(atlas dbusers create "$@")
+  if [ $? -ne 0 ]; then
+    echo $res
+  fi
+}
+
+db_user_exists() {
+  res=$(atlas dbusers describe $1 2>&1 >/dev/null)
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+}
+
+# Handle admin db user
+# check in atlas if the user exists
+if db_user_exists "${DB_ROOT_USER}"; then
+  echo "-> user ${DB_ROOT_USER} already exist... updating"
+  db_user_update "${DB_ROOT_USER}" --password "${DB_ROOT_PASS}" --role "dbAdmin@{DB_NAME},readWrite@${DB_NAME}"
+else
+  echo "-> creating user ${DB_ROOT_USER}"
+  db_user_create --username "${DB_ROOT_USER}" --password "${DB_ROOT_PASS}" --role "dbAdmin@{DB_NAME},readWrite@${DB_NAME}"
 fi
 
-# Extract proto and host from address returned
-DB_PROTO=$(echo $DB_ADDRESS | cut -d':' -f1)
-DB_HOST=$(echo $DB_ADDRESS | cut -d'/' -f3)
-echo "-> connection string: [${DB_PROTO}://${DB_USER}:${DB_PASS}@${DB_HOST}]"
+# handle db user
+if db_user_exists "${DB_USER}"; then
+  echo "-> user ${DB_USER} already exist... updating"
+  db_user_update "${DB_USER}" --password "${DB_PASS}" --role "readWrite@${DB_NAME}"
+else
+  echo "-> creating user ${DB_USER}"
+  db_user_create --username "${DB_USER}" --password "${DB_PASS}" --role "readWrite@${DB_NAME}"
+fi
 
-cat > /run/secrets/output<<EOF
-services: atlas: {
-  address: "${DB_HOST}"
-  secrets: ["user"]
-  ports: "27017"
-  data: {
-    proto: "${DB_PROTO}"
-    dbName: "${DB_NAME}"
-  }
-}
-EOF
+render_service ${CLUSTER_NAME}
