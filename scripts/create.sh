8a7ad0eb5026eb7634e1593ea31fab5924cd2abe
Luc <luc.juggery@gmail.com>
2023-12-12 23:06:04 +0100

    Use acorn external ID to keep track of the cluster (#7)
    * Use acorn external ID to keep track of the cluster
    
    * Prevent deletion of a user if one with the same name exist before
    
    * variables to ease testing
    
    * still wip

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,101 +1,29 @@
 #!/bin/sh
-# set -eo pipefail
+# set -eox pipefail
 
-echo "-> [create.sh]"
+echo "[create.sh]"
 
-# Make sure this script only repply on an Acorn creation event
-if [ "${ACORN_EVENT}" == "delete" ]; then
-   echo "ACORN_EVENT must be [create,update], currently is [${ACORN_EVENT}]"
+# Couple of variables to make local testing simpler
+termination_log="/dev/termination-log"
+acorn_output="/run/secrets/output"
+
+# Make sure this script is only triggered on Acorn creation and update events
+echo "event: ${ACORN_EVENT}"
+if [ "${ACORN_EVENT}" = "delete" ]; then
+   echo "ACORN_EVENT must be [create/update], currently is [${ACORN_EVENT}]"
    exit 0
 fi
 
-render_service() {
-  local CLUSTER_NAME=$1
-  local db_name=${DB_NAME}
-
-  DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
-  DB_PROTO=$(echo $DB_ADDRESS | cut -d':' -f1)
-  DB_HOST=$(echo $DB_ADDRESS | cut -d'/' -f3)
-  echo "DB_ADDRESS: [${DB_ADDRESS}] / DB_PROTO:[${DB_PROTO}] / DB_HOST:[${DB_HOST}]"
-
-  cat > /run/secrets/output<<EOF
-  services: atlas: {
-    address: "${DB_HOST}"
-    default: true
-    secrets: ["admin", "user"]
-    ports: "27017"
-    data: {
-      proto: "${DB_PROTO}"
-      dbName: "${db_name}"
-    }
-  }
-EOF
-}
+# Keep track of the resources created
+created_db_root_user=""
+created_db_user=""
+created_cluster=""
 
 disk_size_arg() {
   if [ -n "${DISK_SIZE_GB}" ]; then
-    echo "--diskSizeGB ${DISK_SIZE_GB}"
+    echo "diskSizeGB ${DISK_SIZE_GB}"
   fi
 }
-
-# Check if cluster with that name already exit
-atlas cluster get ${CLUSTER_NAME} 2>/dev/null
-if [ $? -ne 0 ]; then
-  echo "-> cluster ${CLUSTER_NAME} does not exist"
-
-  disk_arg=$(disk_size_arg)
-  # Create a cluster in the current project
-  echo "-> about to create cluster ${CLUSTER_NAME} of type ${TIER} in ${PROVIDER} / ${REGION}"
-  result=$(atlas cluster create ${CLUSTER_NAME} \
-          --region $REGION --provider $PROVIDER \
-          --tier $TIER --tag creator=acorn_service \
-          --mdbVersion $DB_VERSION --${disk_arg} 2>&1)
-  
-  # Make sure the cluster was created correctly
-  if [ $? -ne 0 ]; then
-    echo $result
-    echo $result > /dev/termination_log
-    exit 1
-  fi
-  
-  # Wait for Atlas to provide cluster's connection string
-  echo "-> waiting for database address"
-  while true; do
-    DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
-    if [ "${DB_ADDRESS}" = "null" ]; then
-        sleep 2
-        echo "... retrying"
-    else
-      break
-    fi
-  done
-else
-  echo "-> cluster ${CLUSTER_NAME} already exist"
-
-  if atlas cluster get ${CLUSTER_NAME} -o json 2>&1 | grep ${TIER} > /dev/null ; then
-    echo "-> cluster ${CLUSTER_NAME} already has the correct tier"
-  else
-    echo "-> updating cluster ${CLUSTER_NAME} of type ${TIER} in ${PROVIDER} / ${REGION}"
-    disk_arg=$(disk_size_arg)
-    echo ${disk_arg}
-    result=$(atlas cluster upgrade "${CLUSTER_NAME}" --tier ${TIER} \
-             --tag creator=acorn_service \
-             --mdbVersion $DB_VERSION \
-             ${disk_arg} 2>&1)
-    if [ $? -ne 0 ]; then
-      echo $result
-      echo $result > /dev/termination_log
-      exit 1
-    fi
-  fi
-fi
-
-# Allow database network access from current IP
-echo "-> allowing connection from current IP address"
-res=$(atlas accessList create --currentIp)
-if [ $? -ne 0 ]; then
-  echo $res
-fi
 
 db_user_update() {
   res=$(atlas dbusers update $@)
@@ -112,12 +40,63 @@ db_user_create() {
 }
 
 db_user_exists() {
-  res=$(atlas dbusers describe $1 2>&1 >/dev/null)
+  atlas dbusers describe $1 2>&1 >/dev/null
   if [ $? -ne 0 ]; then
-    echo ${res} > /dev/termination_log
     return 1
   fi
 }
+
+# Use uppercase values for TIER / PROVIDER / REGION in case user specified lowercase ones
+TIER=$(echo $TIER | tr a-z A-Z)
+PROVIDER=$(echo $PROVIDER | tr a-z A-Z)
+REGION=$(echo $REGION | tr a-z A-Z)
+
+# Check if cluster with that name already exit
+atlas cluster get ${CLUSTER_NAME} 2>/dev/null
+if [ $? -eq 0 ]; then
+  echo "-> cluster ${CLUSTER_NAME} already exist" | tee ${termination_log}
+  exit 1
+fi
+echo "-> cluster ${CLUSTER_NAME} does not exist"
+
+disk_arg=$(disk_size_arg)
+
+# Create a cluster in the current project
+echo "-> about to create cluster ${CLUSTER_NAME} of type ${TIER} in ${PROVIDER} / ${REGION}"
+result=$(atlas cluster create ${CLUSTER_NAME} \
+  --region $REGION --provider $PROVIDER \
+  --tier $TIER --tag creator=acorn_service \
+  --tag acornid=${ACORN_EXTERNAL_ID} \
+  --mdbVersion $DB_VERSION --${disk_arg} 2>&1)
+
+# Make sure the cluster was created correctly
+if [ $? -ne 0 ]; then
+  echo $result
+  echo $result | tee ${termination_log}
+  exit 1
+fi
+
+# Keep track of the created cluster
+created_cluster="${CLUSTER_NAME}"
+
+# Wait for Atlas to provide cluster's connection string
+echo "-> waiting for database address"
+while true; do
+  DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
+  if [ "${DB_ADDRESS}" = "null" ]; then
+      sleep 2
+      echo "... retrying"
+  else
+    break
+  fi
+done
+
+# Allow database network access from current IP
+echo "allowing connection from current IP address"
+res=$(atlas accessList create --currentIp)
+if [ $? -ne 0 ]; then
+  echo $res
+fi
 
 # Handle admin db user
 # check in atlas if the user exists
@@ -127,6 +106,7 @@ if db_user_exists "${DB_ROOT_USER}"; then
 else
   echo "-> creating user ${DB_ROOT_USER}"
   db_user_create --username "${DB_ROOT_USER}" --password "${DB_ROOT_PASS}" --role "dbAdmin@${DB_NAME},readWrite@${DB_NAME}"
+  created_db_root_user=${DB_ROOT_USER}
 fi
 
 # handle db user
@@ -136,6 +116,32 @@ if db_user_exists "${DB_USER}"; then
 else
   echo "-> creating user ${DB_USER}"
   db_user_create --username "${DB_USER}" --password "${DB_PASS}" --role "readWrite@${DB_NAME}"
+  created_db_user=${DB_USER}
 fi
 
-render_service ${CLUSTER_NAME}
+# Get database connection info
+DB_ADDRESS=$(atlas cluster describe ${CLUSTER_NAME} -o json | jq -r .connectionStrings.standardSrv)
+DB_PROTO=$(echo $DB_ADDRESS | cut -d':' -f1)
+DB_HOST=$(echo $DB_ADDRESS | cut -d'/' -f3)
+echo "DB_ADDRESS: [${DB_ADDRESS}] / DB_PROTO:[${DB_PROTO}] / DB_HOST:[${DB_HOST}]"
+
+# Render service
+cat > ${acorn_output}<<EOF
+services: atlas: {
+  address: "${DB_HOST}"
+  default: true
+  secrets: ["admin", "user"]
+  ports: "27017"
+  data: {
+    proto: "${DB_PROTO}"
+    dbName: "${DB_NAME}"
+  }
+}
+secrets: state: {
+  data: {
+    created_cluster: "${created_cluster}"
+    created_db_user: "${created_db_user}"
+    created_db_root_user: "${created_db_root_user}"
+  }
+}
+EOF
--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,31 +1,49 @@
 #!/bin/sh
 # set -eo pipefail
 
-echo "-> [delete.sh]"
+echo "[delete.sh]"
 
-# Make sure this script only repply on an Acorn deletion event
+# Make sure this script only replies to an Acorn deletion event
 if [ "${ACORN_EVENT}" != "delete" ]; then
    echo "ACORN_EVENT must be [delete], currently is [${ACORN_EVENT}]"
    exit 0
 fi
 
-# Make sure the cluster exists
-atlas cluster get ${CLUSTER_NAME} 2>/dev/null
-if [ $? -ne 0 ]; then
-  echo "cluster ${CLUSTER_NAME} does not exist"
-  exit 0
+# Delete the cluster (if created by this service)
+if [ "${CREATED_CLUSTER}" != "" ]; then
+  echo "deleting cluster ${CREATED_CLUSTER}"
+  res=$(atlas cluster delete --force ${CLUSTER_NAME})
+  if [ $? -ne 0 ]; then
+    echo "error deleting cluster: $res"
+  else
+    echo "cluster deleted" 
+  fi
+else
+  echo "no cluster created by the service"
 fi
 
-# Delete the cluster
-echo "-> deleting cluster ${CLUSTER_NAME}"
-res=$(atlas cluster delete --force ${CLUSTER_NAME})
-if [ $? -ne 0 ]; then
-  echo $res
-fi
+# Delete root user if created by this service
+if [ "${CREATED_DB_ROOT_USER}" != "" ]; then
+  echo "deleting db root user ${CREATED_DB_ROOT_USER}"
+  res=$(atlas dbusers delete --force ${CREATED_DB_ROOT_USER})
+  if [ $? -ne 0 ]; then
+    echo "error deleting dbuser: $res"
+  else
+    echo "db root user deleted"
+  fi
+else
+  echo "no root user to delete as none was created by this service"
+fi 
 
-# Delete user
-echo "-> deleting associated user ${DB_USER}"
-res=$(atlas dbusers delete --force ${DB_USER})
-if [ $? -ne 0 ]; then
-  echo $res
-fi
+# Delete user if created by this service
+if [ "${CREATED_DB_USER}" != "" ]; then
+  echo "deleting db user ${CREATED_DB_USER}"
+  res=$(atlas dbusers delete --force ${CREATED_DB_USER})
+  if [ $? -ne 0 ]; then
+    echo "error deleting dbuser: $res"
+  else
+    echo "db user deleted"
+  fi
+else
+  echo "no user to delete as none was created by this service"
+fi 
